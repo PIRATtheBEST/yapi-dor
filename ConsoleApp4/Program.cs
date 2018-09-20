614e490443b1cba455f4842c6292879c12858463
PIRATtheBEST <alexalex0072200722@gmail.com>
2018-09-20 21:42:46 +0300

    я сделал

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            double d, x1, x2;
+            Console.WriteLine("введите числа a,b,c");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("ваше уравнение:" + a + "x^2+" + b + "x+" + "c");
+            d = b ^ 2 - 4 * a * c;
+            x1 = Convert.ToDouble(-b + Math.Sqrt(d));
+            x2 = Convert.ToDouble(-b - Math.Sqrt(d));
+
+            Console.WriteLine("ваши корни:" + x1 + "и" + x1);
+            Console.ReadKey();
+
+
+        }
+    }
+}
