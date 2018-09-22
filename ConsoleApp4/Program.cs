97fa2e7a71a2705216e04aff2857997a7e1b61f2
PIRATtheBEST <alexalex0072200722@gmail.com>
2018-09-23 00:12:28 +0300

    2

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -6,21 +6,34 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            int a, b, c;
-            double d, x1, x2;
             Console.WriteLine("введите числа a,b,c");
+            int a;
             a = Convert.ToInt32(Console.ReadLine());
+            int b;
             b = Convert.ToInt32(Console.ReadLine());
+            int c;
             c = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("ваше уравнение:" + a + "x^2+" + b + "x+" + "c");
+            double d;
             d = b ^ 2 - 4 * a * c;
-            x1 = Convert.ToDouble(-b + Math.Sqrt(d));
-            x2 = Convert.ToDouble(-b - Math.Sqrt(d));
+            if (d < 0) 
+                Console.WriteLine("d=" + d + " следовательно решений нет");
+            else if(d > 0)
+            {
+                    double x1 , x2;
+                    x1 = (-b + Math.Sqrt(d))/2*a;
+                    x2 = (-b - Math.Sqrt(d))/2*a;
+                x1 = int.TryParse(x1);
+                    Console.WriteLine("ваши корни:" + x1 + " и " + x1);
+            }
+            else if  (!(d < 0) && !(d > 0))
+            {
+                double x;               
+                x = -b / 2 * a;
+                Console.WriteLine("ваш корень:" + x);
+            }
 
-            Console.WriteLine("ваши корни:" + x1 + "и" + x1);
-            Console.ReadKey();
-
-
+            Console.ReadKey();        
         }
     }
 }
